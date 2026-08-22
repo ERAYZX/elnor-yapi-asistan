6c01f972a6ae279ca81129e71f9907e852599111
ERAYZX <erayondin54@gmail.com>
2026-08-23 01:15:57 +0300

    CORS izni eklendi, guvenlik kapisi acildi

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from config import Config
 from .database import init_db
+from flask_cors import CORS
 
 def create_app():
     # Uygulamayı ve ayarları başlat
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(Config)
 
     # Veritabanı tablosunu hazırla
